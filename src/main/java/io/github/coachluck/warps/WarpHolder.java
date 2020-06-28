13acaa33b4d52257ec35adeba4289676dfb2062b
CoachLuck <beastlyaj01@gmail.com>
2020-06-28 15:08:22 -0400

    Added display name and default constructor

--- src/main/java/io/github/coachluck/warps/WarpHolder.java
+++ src/main/java/io/github/coachluck/warps/WarpHolder.java
@@ -1,29 +1,43 @@
+/*
+ *     File: WarpHolder.java
+ *     Last Modified: 6/28/20, 12:31 PM
+ *     Project: EssentialServer
+ *     Copyright (C) 2020 CoachL_ck
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package io.github.coachluck.warps;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.bukkit.Location;
 import org.bukkit.Sound;
 
 public class WarpHolder {
 
-    private Location location;
-    private Sound warpSound;
-    private String warpMessage;
+    @Getter @Setter Location location;
+    @Getter @Setter Sound warpSound;
+    @Getter @Setter String warpMessage;
+    @Getter @Setter String displayName;
 
-    public WarpHolder(Location location, Sound warpSound, String warpMessage) {
+    public WarpHolder(Location location, Sound warpSound, String warpMessage, String displayName) {
         this.location = location;
         this.warpSound = warpSound;
         this.warpMessage = warpMessage;
+        this.displayName = displayName;
     }
 
-    public Location getLocation() {
-        return location;
-    }
-
-    public Sound getWarpSound() {
-        return warpSound;
-    }
-
-    public String getWarpMessage() {
-        return warpMessage;
-    }
+    public WarpHolder() { }
 }
