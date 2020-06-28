ac42115a59e176e91b1056f1692f86f1c4d3efb6
CoachLuck <beastlyaj01@gmail.com>
2020-06-28 15:10:20 -0400

    Reformatted

--- src/main/java/io/github/coachluck/commands/DelWarp.java
+++ src/main/java/io/github/coachluck/commands/DelWarp.java
@@ -1,3 +1,23 @@
+/*
+ *     File: DelWarp.java
+ *     Last Modified: 6/28/20, 2:30 PM
+ *     Project: EssentialServer
+ *     Copyright (C) 2020 CoachL_ck
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package io.github.coachluck.commands;
 
 import io.github.coachluck.EssentialServer;
@@ -12,7 +32,7 @@ import java.util.Collections;
 import java.util.List;
 
 public class DelWarp implements CommandExecutor, TabCompleter {
-    private EssentialServer plugin;
+    private final EssentialServer plugin;
 
     public DelWarp(EssentialServer plugin) {
         this.plugin = plugin;
@@ -20,21 +40,19 @@ public class DelWarp implements CommandExecutor, TabCompleter {
 
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String s, String[] args) {
-        if(sender.hasPermission("essentialserver.delwarp")) {
-            if(args.length != 1) {
-                ChatUtils.msg(sender, "&cIncorrect usage: &e/delwarp &b<WarpName>");
-                return true;
-            } else {
-                String warpName = args[0].toLowerCase();
-                if(plugin.warpMap.containsKey(warpName)) {
-                    plugin.warpFile.removeWarp(warpName);
-                    plugin.reloadWarpsMap();
-                    ChatUtils.msg(sender, "&7You have &cremoved &7warp: &e" + warpName);
-                } else {
-                    ChatUtils.msg(sender, "&cCould not find warp &e" + warpName);
-                }
-            }
+        if(args.length != 1) {
+            ChatUtils.msg(sender, "&cIncorrect usage: &e/delwarp &7<&bWarpName&7>");
+            return true;
         }
+
+        String warpName = args[0].toLowerCase();
+        if(!plugin.warpMap.containsKey(warpName)) {
+            ChatUtils.msg(sender, "&cCould not find warp &e" + warpName);
+            return true;
+        }
+        plugin.getWarpFile().removeWarp(warpName);
+        plugin.reloadWarpsMap();
+        ChatUtils.msg(sender, "&7You have &cremoved &7warp: &e" + warpName);
         return true;
     }
 
