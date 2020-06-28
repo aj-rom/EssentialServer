c2be4a9bc1ba9db849214d04262e301972713cc1
CoachLuck <beastlyaj01@gmail.com>
2020-06-28 15:12:34 -0400

    Reformatted and a new Cooldown system

--- src/main/java/io/github/coachluck/commands/Teleport.java
+++ src/main/java/io/github/coachluck/commands/Teleport.java
@@ -1,22 +1,40 @@
+/*
+ *     File: Teleport.java
+ *     Last Modified: 6/28/20, 2:28 PM
+ *     Project: EssentialServer
+ *     Copyright (C) 2020 CoachL_ck
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package io.github.coachluck.commands;
 
 import io.github.coachluck.EssentialServer;
 import io.github.coachluck.utils.ChatUtils;
+import io.github.coachluck.utils.Cooldown;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.scheduler.BukkitRunnable;
 
 import java.util.HashMap;
 import java.util.UUID;
 
 public class Teleport implements CommandExecutor {
-    public static HashMap<UUID, Integer> cooldownTime  = new HashMap<>();
-    public static HashMap<UUID, BukkitRunnable> cooldownTask  = new HashMap<>();
-    private int COOL_DOWN;
-    private EssentialServer plugin;
+    public static HashMap<UUID, Cooldown> cooldowns = new HashMap<>();
+    private final EssentialServer plugin;
 
     public Teleport(EssentialServer plugin) {
         this.plugin = plugin;
@@ -26,65 +44,74 @@ public class Teleport implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         String tpOtherMsg = plugin.getConfig().getString("teleport.others-message");
         String tpMsg = plugin.getConfig().getString("teleport.message");
-        String coolDownMsg = plugin.getConfig().getString("teleport.cooldown-message");
+
         String offlinePlayer = plugin.getConfig().getString("offline-player");
-        COOL_DOWN = plugin.getConfig().getInt("teleport.cooldown-time");
         boolean enableMsg = plugin.getConfig().getBoolean("teleport.message-enable");
+        if(!(sender instanceof Player)) {
+            ChatUtils.logMsg("&cYou must be a player to use this command!");
+            return true;
+        }
 
-        if (sender instanceof Player && sender.hasPermission("essentialserver.tp")) {
-            Player player = (Player) sender;
-            UUID pUUID = player.getUniqueId();
-            if(cooldownTime.containsKey(pUUID)) {
-                String rem = cooldownTime.get(pUUID).toString();
-                ChatUtils.msg(player, coolDownMsg.replaceAll("%time%", rem));
+        Player player = (Player) sender;
+        if (args.length == 0 ) {
+            ChatUtils.msg(player, "&cInsufficient arguments! &7Please try again.");
+            ChatUtils.msg(player, "&cTo teleport yourself: &e/tp &c<&botherplayer&c>");
+            if (player.hasPermission("essentialserver.tp.others")) {
+                ChatUtils.msg(player, "&cTo teleport others: &e/tp &c<&bplayer&c> <&botherplayer&c>");
             }
-            else if (args.length == 0 ) {
-                ChatUtils.msg(player, "&cInsufficient arguments! &7Please try again.");
-                ChatUtils.msg(player, "&cTo teleport yourself: &e/tp &c<&botherplayer&c>");
-                if (player.hasPermission("essentialserver.tp.others")) {
-                    ChatUtils.msg(player, "&cTo teleport others: &e/tp &c<&bplayer&c> <&botherplayer&c>");
-                }
-            }
-            else if(args.length == 1) {
-                try {
-                    Player target = Bukkit.getPlayer(args[0]); //Get player from command
-                    if (player.getDisplayName().equalsIgnoreCase(target.getDisplayName())) {
-                        ChatUtils.msg(player, plugin.getConfig().getString("teleport.self"));
-                    } else {
-                        if(enableMsg) ChatUtils.msg(player, tpMsg.replaceAll("%player%", target.getDisplayName()));
-                        tPTask(player, target, player);
-                    }
+            return true;
+        }
 
-                } catch (NullPointerException e) {
-                    ChatUtils.msg(player, offlinePlayer.replaceAll("%player%", args[0]));
-                    return true;
-                }
+        if(args.length == 1) {
+            Player target = Bukkit.getPlayer(args[0]);
+            if(target == null) {
+                ChatUtils.msg(player, offlinePlayer.replaceAll("%player%", args[0]));
+                return true;
             }
-            else if(args.length == 2 && player.hasPermission("essentialserver.tp.others")){
-                try{
-                Player playerToSend = Bukkit.getPlayer(args[0]);
-                Player target = Bukkit.getPlayer(args[1]);
-                    if(playerToSend.getDisplayName().equalsIgnoreCase(target.getDisplayName())) {
-                        ChatUtils.msg(sender, "&cDid you really mean to do that? Try again...");
-                        return true;
-                    }
-                    else {
-                        if(enableMsg) ChatUtils.msg(sender, tpOtherMsg
-                                .replaceAll("%player1%", playerToSend.getDisplayName())
-                                .replaceAll("%player2%", target.getDisplayName()));
-                        tPTask(playerToSend, target, player);
-
-                    }
-                }catch (NullPointerException e){
-                    ChatUtils.msg(player, offlinePlayer.replaceAll("%player%", args[1]));
-                }
+            if (player.getDisplayName().equalsIgnoreCase(target.getDisplayName())) {
+                ChatUtils.msg(player, plugin.getConfig().getString("teleport.self"));
+                return true;
             }
-            else if(args.length > 2) {
-                if(sender.hasPermission("essentialserver.tp")) ChatUtils.msg(sender, "&cToo many arguments! Try /tp <player>");
-                if(sender.hasPermission("essentialserver.tp.others")) ChatUtils.msg(sender, "&cToo many arguments! Try /tp <player> <player>");
+            if(tPTask(player, target, player) && enableMsg) {
+                ChatUtils.msg(player, tpMsg.replaceAll("%player%", target.getDisplayName()));
+                return true;
             }
         }
-        else ChatUtils.logMsg("&cYou must be a player to use this command!");
+        else if(args.length == 2) {
+            if(!player.hasPermission("essentialserver.tp.others")) {
+                ChatUtils.msg(sender, "&cToo many arguments! Try /tp <player>");
+                return true;
+            }
+
+            Player playerToSend = Bukkit.getPlayer(args[0]);
+            Player target = Bukkit.getPlayer(args[1]);
+            if(playerToSend == null) {
+                ChatUtils.msg(player, offlinePlayer.replaceAll("%player%", args[0]));
+                return true;
+            }
+            if(target == null) {
+                ChatUtils.msg(player, offlinePlayer.replaceAll("%player%", args[1]));
+                return true;
+            }
+            if(playerToSend.getDisplayName().equalsIgnoreCase(target.getDisplayName())) {
+                ChatUtils.msg(sender, "&cDid you really mean to do that? Try again...");
+                return true;
+            }
+
+            if(tPTask(playerToSend, target, player) && enableMsg) {
+                ChatUtils.msg(sender, tpOtherMsg
+                        .replaceAll("%player1%", playerToSend.getDisplayName())
+                        .replaceAll("%player2%", target.getDisplayName()));
+                return true;
+            }
+        } else {
+            if (sender.hasPermission("essentialserver.tp"))
+                ChatUtils.msg(sender, "&cToo many arguments! Try /tp <player>");
+            if (sender.hasPermission("essentialserver.tp.others"))
+                ChatUtils.msg(sender, "&cToo many arguments! Try /tp <player> <player>");
+
+            return true;
+        }
         return true;
     }
 
@@ -94,24 +121,25 @@ public class Teleport implements CommandExecutor {
      * @param target the player to teleport TO
      * @param sender the person initiating the Teleport
      */
-    private void tPTask(Player playerToSend, Player target, Player sender) {
-        UUID sUUID = sender.getUniqueId();
-        playerToSend.teleport(target.getLocation());
-        if (!sender.hasPermission("essentialserver.tp.bypass") && COOL_DOWN > 0) {
-            cooldownTime.put(sUUID, COOL_DOWN);
-            cooldownTask.put(sUUID, new BukkitRunnable() {
-                @Override
-                public void run() {
-                    cooldownTime.put(sUUID, cooldownTime.get(sUUID) - 1); // decrease the cooldown
-                    if (cooldownTime.get(sUUID) == 0) { // end of cooldown
-                        cooldownTime.remove(sUUID);
-                        cooldownTask.remove(sUUID);
-                        cancel();
-                    }
-                }
-            });
-            cooldownTask.get(sUUID).runTaskTimer(plugin, 20, 20);
+    private boolean tPTask(Player playerToSend, Player target, Player sender) {
+        if(sender.hasPermission("essentialserver.tp.bypass")) {
+            playerToSend.teleport(target.getLocation());
+            return true;
         }
+
+        UUID sUUID = sender.getUniqueId();
+        if(cooldowns.containsKey(sUUID)) {
+            if(cooldowns.get(sUUID).getTimeRemaining() > 0) {
+                int rem = cooldowns.get(sUUID).getTimeRemaining();
+                ChatUtils.msg(sender, plugin.getConfig().getString("teleport.cooldown-message")
+                        .replaceAll("%time%", "" + rem));
+                return false;
+            }
+            cooldowns.remove(sUUID);
+        }
+        playerToSend.teleport(target.getLocation());
+        cooldowns.put(sUUID, new Cooldown(Cooldown.CooldownType.TELEPORT));
+        return true;
     }
 
 }
--- src/main/java/io/github/coachluck/commands/Warp.java
+++ src/main/java/io/github/coachluck/commands/Warp.java
@@ -1,23 +1,48 @@
+/*
+ *     File: Warp.java
+ *     Last Modified: 6/28/20, 2:58 PM
+ *     Project: EssentialServer
+ *     Copyright (C) 2020 CoachL_ck
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package io.github.coachluck.commands;
 
 import io.github.coachluck.EssentialServer;
 import io.github.coachluck.utils.ChatUtils;
-import io.github.coachluck.utils.JsonMessage;
+import io.github.coachluck.utils.Cooldown;
 import io.github.coachluck.warps.WarpHolder;
+import net.md_5.bungee.api.chat.ClickEvent;
+import net.md_5.bungee.api.chat.ComponentBuilder;
+import net.md_5.bungee.api.chat.HoverEvent;
+import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 import static io.github.coachluck.utils.ChatUtils.format;
 
 public class Warp implements CommandExecutor, TabCompleter {
-    private EssentialServer plugin;
+
+    public static HashMap<UUID, Cooldown> cooldowns = new HashMap<>();
+    private final EssentialServer plugin;
 
     public Warp(EssentialServer ins) {
         this.plugin = ins;
@@ -25,55 +50,91 @@ public class Warp implements CommandExecutor, TabCompleter {
 
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-        String noPermWarp = plugin.warpData.getString("messages.no-perm-for-warp");
-        String warpNotFound = plugin.warpData.getString("messages.warp-not-found");
-        List<String> warpListHeader = plugin.warpData.getStringList("messages.warp-list-header");
-        List<String> warpListFooter = plugin.warpData.getStringList("messages.warp-list-footer");
-        String warpListColor = plugin.warpData.getString("messages.warp-list-color");
+        final YamlConfiguration warpData = plugin.getWarpFile().getWarpData();
+        final String noPermWarp = warpData.getString("messages.no-perm-for-warp");
+        final String warpNotFound = warpData.getString("messages.warp-not-found");
+        final List<String> warpListHeader = warpData.getStringList("messages.warp-list-header");
+        final List<String> warpListFooter = warpData.getStringList("messages.warp-list-footer");
+        final String warpListColor = warpData.getString("messages.warp-list-color");
 
         if(sender instanceof Player) {
             Player p = (Player) sender;
             if(args.length > 1) {
                 ChatUtils.msg(p, "&cIncorrect usage: &e/warp &b<WarpName>");
                 return true;
-            } else if(args.length == 1) {
-                String warpName = args[0].toLowerCase();
-                if(plugin.warpFile.getAllWarps().contains(warpName)) {
-                    if(p.hasPermission("warps.*") || p.hasPermission("warps." + warpName)) {
-                        WarpHolder warp = plugin.warpMap.get(warpName);
-                        p.playSound(warp.getLocation(), warp.getWarpSound(), 1.0F, 1.0F);
-                        p.teleport(warp.getLocation());
-                        ChatUtils.msg(p, warp.getWarpMessage());
-                    } else {
-                        ChatUtils.msg(p, noPermWarp.replaceAll("%warp%", warpName));
-                    }
-                } else {
-                    ChatUtils.msg(p, warpNotFound.replaceAll("%warp%", warpName));
-                }
-            } else {
-                JsonMessage warpList = new JsonMessage();
-                ArrayList<String> currentWarps = new ArrayList<>(plugin.warpMap.keySet());
-                Collections.sort(currentWarps);
-                for(String s : currentWarps) {
-                    if(p.hasPermission("warps." + s)) {
-                        warpList.append(format(warpListColor + s
-                                + plugin.warpData.getString("messages.warp-list-separator")))
-                                .setHoverAsTooltip(format("&7Click me to warp to &e" + s))
-                                .setClickAsExecuteCmd("/warp " + s)
-                                .save();
-                    }
-                }
-                // header
-                for(String s : warpListHeader) p.sendMessage(format(s));
-                // body - JsonMessage
-                warpList.send(p);
-                // footer
-                for(String s : warpListFooter) p.sendMessage(format(s));
             }
+            if(args.length == 1) {
+                String warpName = args[0].toLowerCase();
+                if(plugin.warpMap.get(warpName) == null) {
+                    ChatUtils.msg(p, warpNotFound.replaceAll("%warp%", warpName));
+                    return true;
+                }
+                if(!p.hasPermission("warps.*") && !p.hasPermission("warps." + warpName)) {
+                    ChatUtils.msg(p, noPermWarp.replaceAll("%warp%", warpName));
+                    return true;
+                }
+                if(hasCooldown(p)) {
+                    ChatUtils.msg(p, warpData.getString("messages.cooldown")
+                            .replaceAll("%time%", "" + cooldowns.get(p.getUniqueId()).getTimeRemaining()));
+                    return true;
+                }
+
+                WarpHolder warp = plugin.warpMap.get(warpName);
+                p.playSound(warp.getLocation(), warp.getWarpSound(), 1.0F, 1.0F);
+                p.teleport(warp.getLocation());
+                ChatUtils.msg(p, warp.getWarpMessage().replaceAll("%name%", warp.getDisplayName()));
+
+                return true;
+            }
+
+            final List<String> currentWarps = new ArrayList<>(plugin.warpMap.keySet());
+            ComponentBuilder warpList = new ComponentBuilder();
+            Collections.sort(currentWarps);
+            for(String s : currentWarps) {
+                if(p.hasPermission("warps." + s)) {
+                    WarpHolder warpHolder = plugin.warpMap.get(s);
+                    TextComponent warp = new TextComponent(format(warpListColor + s + warpData.getString("messages.warp-list-separator")));
+                            warp.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/warp " + s));
+                            warp.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT,
+                                    new ComponentBuilder(ChatUtils.format("&7Click me to warp to &e" + warpHolder.getDisplayName())).create()));
+                    warpList.append(warp);
+                }
+            }
+            for(String s : warpListHeader)
+                p.sendMessage(format(s));
+
+            p.spigot().sendMessage(warpList.create());
+
+            for(String s : warpListFooter)
+                p.sendMessage(format(s));
+
         } else {
             ChatUtils.logMsg("&cYou must be a player to use this command.");
         }
         return true;
+    }
+
+    /**
+     * Check whether or not a player has a cooldown remaining
+     * @param player the players to check the cooldown of
+     * @return true if they do have a cooldown, false if not
+     */
+    private boolean hasCooldown(Player player) {
+        UUID uuid = player.getUniqueId();
+        if(player.hasPermission("warps.bypass-cooldown"))
+            return false;
+
+        if(cooldowns.get(uuid) != null) {
+            Cooldown cooldown = cooldowns.get(uuid);
+            if(cooldown.getTimeRemaining() != 0)
+                return true;
+
+            cooldowns.remove(uuid);
+            return false;
+        }
+
+        cooldowns.put(uuid, new Cooldown(Cooldown.CooldownType.WARP));
+        return false;
     }
 
     @Override
