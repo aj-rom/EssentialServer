dda4b5418fa0df3d9cd5ac0b57c18ade1ab38424
CoachLuck <beastlyaj01@gmail.com>
2020-06-28 15:04:52 -0400

    New Cooldown System for Warps and Teleports

---
+++ src/main/java/io/github/coachluck/utils/Cooldown.java
@@ -0,0 +1,60 @@
+/*
+ *     File: Cooldown.java
+ *     Last Modified: 6/28/20, 2:28 PM
+ *     Project: EssentialServer
+ *     Copyright (C) 2020 CoachL_ck
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package io.github.coachluck.utils;
+
+import io.github.coachluck.EssentialServer;
+import lombok.Getter;
+import org.bukkit.scheduler.BukkitRunnable;
+
+public class Cooldown {
+
+    public enum CooldownType {
+        TELEPORT,
+        WARP
+    }
+
+    @Getter CooldownType cooldownType;
+    @Getter int timeRemaining;
+
+    public Cooldown(CooldownType type) {
+        EssentialServer plugin = EssentialServer.getPlugin(EssentialServer.class);
+        switch (type) {
+            case TELEPORT:
+                timeRemaining = plugin.getConfig().getInt("teleport.cooldown-time");
+                break;
+            case WARP:
+                timeRemaining = plugin.getWarpFile().getWarpData().getInt("cooldown");
+                break;
+        }
+
+        BukkitRunnable task = new BukkitRunnable() {
+            @Override
+            public void run() {
+                timeRemaining--;
+                if (timeRemaining == 0) {
+                    cancel();
+                }
+            }
+        };
+        task.runTaskTimerAsynchronously(plugin, 20, 20);
+    }
+
+}
