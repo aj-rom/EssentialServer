6d05b898cea4f4dd0fea527e2e4604f7457219e3
CoachLuck <beastlyaj01@gmail.com>
2020-06-28 15:12:02 -0400

    Added pre-existing warp check

--- src/main/java/io/github/coachluck/commands/SetWarp.java
+++ src/main/java/io/github/coachluck/commands/SetWarp.java
@@ -1,3 +1,23 @@
+/*
+ *     File: SetWarp.java
+ *     Last Modified: 6/28/20, 12:45 PM
+ *     Project: EssentialServer
+ *     Copyright (C) 2020 CoachL_ck
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package io.github.coachluck.commands;
 
 import io.github.coachluck.EssentialServer;
@@ -27,10 +47,14 @@ public class SetWarp implements TabCompleter, CommandExecutor {
             Player p = (Player) sender;
             if(args.length == 1) {
                 String warpName = args[0].toLowerCase();
-                if(plugin.warpFile.addWarp(warpName, p.getLocation())) {
-                    ChatUtils.msg(p, plugin.warpData.getString("messages.create-warp").replaceAll("%warp%", warpName));
+                if(plugin.warpMap.get(warpName) != null) {
+                    ChatUtils.msg(p, "&e" + args[0] + " &7already exists! Please use a different name.");
+                    return true;
+                }
+                if(plugin.getWarpFile().addWarp(args[0], p.getLocation())) {
+                    ChatUtils.msg(p, plugin.getWarpFile().getWarpData().getString("messages.create-warp").replaceAll("%warp%", warpName));
                 } else {
-                    ChatUtils.msg(p, plugin.warpData.getString("messages.warp-already-exists").replaceAll("%warp%", warpName));
+                    ChatUtils.msg(p, plugin.getWarpFile().getWarpData().getString("messages.warp-already-exists").replaceAll("%warp%", warpName));
                 }
                 plugin.reloadWarpsMap();
 
